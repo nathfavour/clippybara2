6935e0163c38694aa717ae090fe99909ab8faa3c
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-03-12 17:28:49 +0100

    📝 chore: update Cargo.

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Library for the Clippybara clipboard syncing application.
-
-pub mod api;
-pub mod clipboard;
-pub mod config;
-pub mod qrcode;
-pub mod ui;
--- src/ui/mod.rs
+++
@@ -1,6 +0,0 @@
-//! User interface for the Clippybara application.
-
-pub mod desktop;
-
-#[cfg(feature = "mobile")]
-pub mod mobile;
